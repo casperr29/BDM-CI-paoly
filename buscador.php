e1c33754aef4da5ee7762364ee3edd60f609b119
casperr29 <arelylarag329@gmail.com>
2022-09-16 17:50:03 -0500

    primer avance

--- buscador.php
+++ buscador.php
@@ -6,11 +6,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="shortcut icon" href="img/quran.png">
    <title>buscador</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="cssarely/buscador.css">
 
 </head>
