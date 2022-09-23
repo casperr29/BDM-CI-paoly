cf0985332eb10d486dd6027f4dfdce09d2d35d23
casperr29 <arelylarag329@gmail.com>
2022-09-23 00:43:51 -0500

    Update historial.php

--- historial.php
+++ historial.php
@@ -4,7 +4,7 @@
      <meta charset= "UTF - 8" >
      <meta name= "viewport" content = "width = device - width , initial - scale = 1.0">
      <link rel="shortcut icon" href="img/quran.png">
-     <title>Perfil</title>
+     <title>Historial de compras</title>
      <link rel="stylesheet" type="text/css" href = "cssarely/historial.css"/>
 
 </head>
