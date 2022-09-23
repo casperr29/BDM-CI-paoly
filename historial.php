53bcef510be530553c110e957569ebd50f10ee0f
Paola_Cuellar18 <pao.cuellar18@hotmail.com>
2022-09-23 03:57:52 -0500

    Datos Dummys del historial

--- historial.php
+++ historial.php
@@ -37,6 +37,92 @@
 
       </div>
 
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/Alexander_Hamilton.jpg"></a>
+         <p> Libro : <span>Alexander Hamilton</span> </p>
+         <p> Precio : <span>$500</span> </p>
+         <p> Autor : <span>Ron Chernow</span> </p>
+         <p> Número de pedido : <span>543123</span> </p>
+         <p> Estado del pedido : <span>Entregado</span> </p>
+         <p> Llegó el día <span>20/08/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/entrevista_vampiro.jpg"></a>
+         <p> Libro : <span>Entrevista con el vampiro</span> </p>
+         <p> Precio : <span>$99</span> </p>
+         <p> Autor : <span>Anne Rice</span> </p>
+         <p> Número de pedido : <span>543123</span> </p>
+         <p> Estado del pedido : <span>Entregado</span> </p>
+         <p> Llegó el día <span>20/08/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/marnie.jpg"></a>
+         <p> Libro : <span>The art of: When Marnie was there</span> </p>
+         <p> Precio : <span>$99</span> </p>
+         <p> Autor : <span>Estudio Ghibli</span> </p>
+         <p> Número de pedido : <span>762980</span> </p>
+         <p> Estado del pedido : <span>Pendiente</span> </p>
+         <p> Llega el día <span>10/10/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/castillo_vagabundo.jpg"></a>
+         <p> Libro : <span>El castillo ambulante</span> </p>
+         <p> Precio : <span>$130</span> </p>
+         <p> Autor : <span>Diana Wynne Jones</span> </p>
+         <p> Número de pedido : <span>762980</span> </p>
+         <p> Estado del pedido : <span>Pendiente</span> </p>
+         <p> Llega el día <span>10/10/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/heartstopper.jpg"></a>
+         <p> Libro : <span>Heartstopper Vol. 1</span> </p>
+         <p> Precio : <span>$600</span> </p>
+         <p> Autor : <span>Alice Oseman</span> </p>
+         <p> Número de pedido : <span>982038</span> </p>
+         <p> Estado del pedido : <span>Pendiente</span> </p>
+         <p> Llega el día <span>30/09/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/blue_period.jpg"></a>
+         <p> Libro : <span>Blue Period</span> </p>
+         <p> Precio : <span>$150</span> </p>
+         <p> Autor : <span>Tsubasa Yamaguchi</span> </p>
+         <p> Número de pedido : <span>548239</span> </p>
+         <p> Estado del pedido : <span>Pendiente</span> </p>
+         <p> Llega el día <span>23/10/2022</span> </p>
+
+
+      </div>
+
+      <div class="box">
+        <a href="producto.php"><img class="image" src="img/libro_troll.jpg"></a>
+         <p> Libro : <span>El libro troll</span> </p>
+         <p> Precio : <span>$99</span> </p>
+         <p> Autor : <span>Elrubius</span> </p>
+         <p> Número de pedido : <span>239642</span> </p>
+         <p> Estado del pedido : <span>Pendiente</span> </p>
+         <p> Llega el día <span>25/12/2022</span> </p>
+
+
+      </div>
+
+
+
    </div>
 
 </section>
