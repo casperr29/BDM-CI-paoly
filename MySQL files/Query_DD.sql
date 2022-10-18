3ab27d52bb9b8fd22f0abe5f50bcde7330ca835b
Paola_Cuellar18 <pao.cuellar18@hotmail.com>
2022-10-18 04:05:26 -0500

    Query del diccionario de datos

---
+++ MySQL files/Query_DD.sql
@@ -0,0 +1,27 @@
+USE BDM_CI;
+
+SELECT distinct
+        t.table_schema AS db_name,
+       t.table_name,
+       (CASE WHEN t.table_type = 'BASE TABLE' THEN 'table'
+             WHEN t.table_type = 'VIEW' THEN 'view'
+             ELSE t.table_type
+        END) AS table_type,
+        c.column_name,
+        c.column_type,
+        c.column_default,
+        c.column_key,
+        c.is_nullable,
+        c.extra,
+        c.column_comment
+FROM information_schema.tables AS t
+INNER JOIN information_schema.columns AS c
+ON t.table_name = c.table_name
+AND t.table_schema = c.table_schema
+WHERE t.table_type IN ('BASE TABLE', 'VIEW')
+AND t.table_schema = 'BDM_CI'
+ORDER BY
+        c.column_name,
+        t.table_schema,
+         t.table_name,
+         c.ordinal_position;
